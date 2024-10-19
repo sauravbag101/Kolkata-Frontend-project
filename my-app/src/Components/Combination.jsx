725cac17db38f5ec958a34b6a19e1f4113fab5b4
sauravbag101 <sauravbag101@gmail.com>
2024-10-19 16:40:04 +0530

    rrrr

--- my-app/src/Components/Combination.jsx
+++ my-app/src/Components/Combination.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { FaAngleRight } from "react-icons/fa6";
-import { AiOutlineDelete } from "react-icons/ai";
-import { ToggleSlider }  from "react-toggle-slider";
+
 import { Link } from 'react-router-dom';
 
 const Combination = () => {
--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,8 +1,20 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// });
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-toggle-slider'],
+    },
+  },
 });
-
